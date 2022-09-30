0a825c3078511e9f8cd35b781646418b0e38eedb
SivaranjaniSuresh <s.si@northeastern.edu>
2022-09-30 19:23:40 -0400

    update class

---
+++ hrManagement/src/manager/EmpHistory.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package manager;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author sivaranjanisuresh
+ */
+public class EmpHistory {
+    
+    private ArrayList<EmpDeclaration> history;
+    
+    public EmpHistory(){
+        this.history = new ArrayList<EmpDeclaration>();
+    }
+
+    public ArrayList<EmpDeclaration> getHistory() {
+        return history;
+    }
+
+    public void setHistory(ArrayList<EmpDeclaration> history) {
+        this.history = history;
+    }
+    
+    
+
+  
+    
+    
+    
+    
+}
+
+    
